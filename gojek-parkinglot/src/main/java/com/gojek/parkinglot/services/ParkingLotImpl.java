80740acc3b8158cff241a88204d74a13a10639af
shahid <shahid@ubuntu>
2019-07-01 10:16:03 -0700

    Implemented Parking Lot API

---
+++ gojek-parkinglot/src/main/java/com/gojek/parkinglot/services/ParkingLotImpl.java
@@ -0,0 +1,120 @@
+package com.gojek.parkinglot.services;
+
+import com.gojek.parkinglot.api.ParkingLot;
+import com.gojek.parkinglot.builder.Vehicle;
+import com.gojek.parkinglot.builder.impl.ParkingSlots;
+import com.gojek.parkinglot.builder.impl.Slot;
+import com.gojek.parkinglot.builder.impl.Car;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.IntStream;
+
+public class ParkingLotImpl implements ParkingLot{
+	
+	
+	private ParkingSlots parkingSlots;
+	
+	public ParkingLotImpl() {
+		parkingSlots = new ParkingSlots();
+	}
+	
+	@Override
+	public boolean createParkingLot(int totalNumSlots) {
+		
+		try{
+			parkingSlots.getAvailableSlots().clear();
+			parkingSlots.getOccupiedSlots().clear();
+			IntStream.range(1,totalNumSlots+1).forEach((n)->parkingSlots.getAvailableSlots().add(new Slot(n)));
+		}
+		catch(Exception ex){
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public ParkingSlots getParkingSlots() {
+		
+		return this.parkingSlots;
+	}
+
+	@Override
+	public Slot assignSlot(String colour,String regNum) {
+		
+		Vehicle v = new Car(colour,regNum);
+		if(parkingSlots.getAvailableSlots().isEmpty())
+			return null;
+		Slot nextNearestAvilSlot = parkingSlots.getAvailableSlots().poll();
+		parkingSlots.getOccupiedSlots().add(nextNearestAvilSlot, v);
+		return nextNearestAvilSlot;
+		
+	}
+	
+	// in case of null return. either no Vehicle is parked or that slot is not available
+	@Override
+	public Slot markSlotFree(Slot slot) {
+		
+		Slot freeSlot = parkingSlots.getOccupiedSlots().remove(slot);
+		if(freeSlot != null){
+			parkingSlots.getAvailableSlots().add(freeSlot);
+		}
+		return freeSlot;
+	}
+
+	@Override
+	public List<String> getRegNumByColour(String colour) {
+		
+		List<String> regNumberByColour = new ArrayList<String>();
+		ArrayList<ArrayList<String>> temp = new ArrayList<ArrayList<String>>();
+		Map<String,Slot> regNumToSlot = parkingSlots.getOccupiedSlots().getRegNumberToSlot();
+		Map<Slot,Vehicle> slotToVehicle = parkingSlots.getOccupiedSlots().getSlotToVehicle();
+		for(Entry<String,Slot> regNumToSlotEntry : regNumToSlot.entrySet()){
+			
+			if(slotToVehicle.get(regNumToSlotEntry.getValue()).getColour().equals(colour)){
+				//regNumberByColour.add(regNumToSlotEntry.getKey());
+				ArrayList<String> al = new ArrayList<String>();
+				al.add(regNumToSlotEntry.getKey());
+				al.add(String.valueOf(regNumToSlotEntry.getValue().getId()));
+				temp.add(al);
+			}
+			
+		}
+		Collections.sort(temp,(x,y) -> x.get(1).compareTo(y.get(1)));
+		temp.forEach((list)-> regNumberByColour.add(list.get(0)));
+		
+		return regNumberByColour;
+	}
+
+	@Override
+	public Slot getSlotNumByRegNum(String regNum) {
+		
+		if(!parkingSlots.getOccupiedSlots().getRegNumberToSlot().containsKey(regNum))
+			return null;
+		
+		return parkingSlots.getOccupiedSlots().getRegNumberToSlot().get(regNum);
+	}
+
+	@Override
+	public List<Slot> getSlotByColour(String colour) {
+		
+		List<Slot> slotByColour = new ArrayList<Slot>();
+		Map<Slot,Vehicle> slotToVehicle = parkingSlots.getOccupiedSlots().getSlotToVehicle();
+		for(Entry<Slot,Vehicle> slotToVehicleEntry : slotToVehicle.entrySet()){
+			
+			if(slotToVehicleEntry.getValue().getColour().equals(colour)){
+				slotByColour.add(slotToVehicleEntry.getKey());
+			}
+			
+		}
+		
+		return slotByColour;
+	}
+	
+	
+	
+
+}
---
+++ gojek-parkinglot/src/test/java/com/gojek/parkinglot/services/ParkingLotImpltest.java
@@ -0,0 +1,99 @@
+package com.gojek.parkinglot.services;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.gojek.parkinglot.api.ParkingLot;
+import com.gojek.parkinglot.builder.impl.ParkingLotSingleton;
+import com.gojek.parkinglot.builder.impl.Slot;
+
+public class ParkingLotImpltest {
+	
+	ParkingLot parkingLot;
+	
+	@BeforeMethod
+	public void setup(){
+		parkingLot = ParkingLotSingleton.getParkingLotInstance();
+		parkingLot.createParkingLot(0);
+	}
+	
+	@Test
+	public void createParkingLotTest(){
+		
+		AssertJUnit.assertTrue(parkingLot.getParkingSlots().getAvailableSlots().size() == 0);
+		AssertJUnit.assertTrue(parkingLot.getParkingSlots().getOccupiedSlots().size() == 0);
+		parkingLot.createParkingLot(6);
+		AssertJUnit.assertTrue(parkingLot.getParkingSlots().getAvailableSlots().size() == 6);
+		AssertJUnit.assertTrue(parkingLot.getParkingSlots().getOccupiedSlots().size() == 0);
+		parkingLot.createParkingLot(0);
+		AssertJUnit.assertTrue(parkingLot.getParkingSlots().getAvailableSlots().size() == 0);
+		AssertJUnit.assertTrue(parkingLot.getParkingSlots().getOccupiedSlots().size() == 0);
+	}
+	
+	@Test
+	public void assignAndMarkSlotTest(){
+		
+		parkingLot.createParkingLot(6);
+		IntStream.range(1,6).forEach((i)->parkingLot.assignSlot("Blue",String.valueOf(i)));
+		AssertJUnit.assertTrue(parkingLot.getParkingSlots().getAvailableSlots().size() == 1);
+		AssertJUnit.assertTrue(parkingLot.getParkingSlots().getOccupiedSlots().size() == 5);
+		parkingLot.assignSlot("Blue","6");
+		AssertJUnit.assertTrue(parkingLot.assignSlot("Blue","7") == null);
+		parkingLot.markSlotFree(new Slot(1));
+		AssertJUnit.assertTrue(parkingLot.getParkingSlots().getOccupiedSlots().size() == 5);
+		parkingLot.assignSlot("Brown","7");
+		AssertJUnit.assertTrue(parkingLot.getParkingSlots().getOccupiedSlots().getRegNumberToSlot().get("7").equals(new Slot(1)));
+		
+	}
+	
+	@Test
+	public void getRegNumByColourTest(){
+		
+		parkingLot.createParkingLot(6);
+		IntStream.range(1,5).forEach((i)->parkingLot.assignSlot("Blue",String.valueOf(i)));
+		parkingLot.assignSlot("Black","5");
+		parkingLot.assignSlot("Blue","6");
+		parkingLot.getRegNumByColour("Blue");
+		List<String> regNumberByColour = parkingLot.getRegNumByColour("Blue");
+		AssertJUnit.assertTrue(regNumberByColour.size() == 5);
+		AssertJUnit.assertTrue(regNumberByColour.get(4).equals("6"));
+		parkingLot.markSlotFree(new Slot(1));
+		regNumberByColour = parkingLot.getRegNumByColour("Blue");
+		AssertJUnit.assertTrue(regNumberByColour.size() == 4);
+		AssertJUnit.assertTrue(regNumberByColour.get(3).equals("6"));
+	
+	}
+	
+	@Test
+	public void getSlotNumByRegNumTest(){
+		
+		parkingLot.createParkingLot(6);
+		IntStream.range(1,5).forEach((i)->parkingLot.assignSlot("Blue",String.valueOf(i)));
+		AssertJUnit.assertTrue(parkingLot.getSlotNumByRegNum("2").equals(new Slot(2)));
+		AssertJUnit.assertFalse(parkingLot.getSlotNumByRegNum("2").equals(new Slot(6)));
+		AssertJUnit.assertTrue(parkingLot.getSlotNumByRegNum("12") == null);
+		
+	}
+	
+	@Test
+	public void getSlotByClourTest(){
+		
+		parkingLot.createParkingLot(6);
+		IntStream.range(1,5).forEach((i)->parkingLot.assignSlot("Blue",String.valueOf(i)));
+		parkingLot.assignSlot("Black","5");
+		parkingLot.assignSlot("Blue","6");
+		parkingLot.getRegNumByColour("Blue");
+		AssertJUnit.assertTrue(parkingLot.getSlotByColour("Blue").size() == 5);
+		parkingLot.markSlotFree(new Slot(1));
+		parkingLot.assignSlot("Black","7");
+		AssertJUnit.assertTrue(parkingLot.getSlotByColour("Blue").size() == 4);
+		AssertJUnit.assertTrue(parkingLot.getSlotByColour("Black").size() == 2);
+		AssertJUnit.assertTrue(parkingLot.getParkingSlots().getOccupiedSlots().getSlotToVehicle().get(new Slot(1)).getColour().equals("Black"));
+
+	}
+	
+}
