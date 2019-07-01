c77ee95b4297e73dfd7ab38bf3c98bdccbc891b7
shahid <shahid@ubuntu>
2019-07-01 10:13:48 -0700

    Added Constant class

---
+++ gojek-parkinglot/src/main/java/com/gojek/parkinglot/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.gojek.parkinglot.constants;
+
+/**
+ * All the constant used in this project
+ * @author shahid
+ *
+ */
+public class Constants {
+	
+	public static final int DEFAULT_SIZE = 2;
+	
+	public static final int DEFAULT_HASHCODE = 0;
+
+}
